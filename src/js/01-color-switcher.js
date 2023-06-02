6ecf67bc993edebef86a6a9f007a512348565217
vovaprodan <121820923+vovaprodan@users.noreply.github.com>
2023-06-02 18:15:16 +0200

    рпьаь

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body')
 
+
+
+buttonStart.addEventListener('click', onClickBtn)
+
+
+function onClickBtn(event) {
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+    timerId = setInterval(() => {
+     body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+buttonStop.addEventListener("click", () => {
+  clearInterval(timerId);
+
+});
