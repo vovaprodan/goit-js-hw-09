a2b53e4e55abf8c268cdf43e1badb95bbc4cdd34
vovaprodan <121820923+vovaprodan@users.noreply.github.com>
2023-06-02 16:15:45 +0000

    Deploying to gh-pages from @ vovaprodan/goit-js-hw-09@6ecf67bc993edebef86a6a9f007a512348565217 🚀

---
+++ 01-color-switcher.437187dd.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(t){timerId=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId)}))}();
+//# sourceMappingURL=01-color-switcher.437187dd.js.map
---
+++ 01-color-switcher.d545985c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(t){timerId=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.d545985c.js.map
